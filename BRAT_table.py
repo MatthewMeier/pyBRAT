acfe9446c1e7541f9b29fd132bbbabdd780e1d31
Matthew Meier <matthew.meier@usu.edu>
2018-10-23 16:02:47 -0600

    Revert "Added an extra message"
    This reverts commit f03bb97b6495993aaa8ea17878e3c2ef3da05043.

--- BRAT_table.py
+++ BRAT_table.py
@@ -51,7 +51,6 @@ def main(
     #arcpy.env.workspace = scratch
     arcpy.env.overwriteOutput = True
     arcpy.CheckOutExtension("Spatial")
-    arcpy.AddMessage("Hey")
 
     # --check input projections--
     validate_inputs(seg_network, road, railroad, canal, is_verbose)
